a5f106a5ff85331b7ccdec7f8cbb07ebac9f2e67
Youssefamenallah23 <ameny688@gmail.com>
2025-10-20 16:37:15 +0100

    hero scroll

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,6 +5,8 @@ import { useRef } from "react";
 import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+  const videoRef = useRef();
+  const isMobile = useMediaQuery({ maxWidth: 767 });
   useGSAP(() => {
     const heroSplit = new SplitText(".title", { type: "chars,words" });
     const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
@@ -35,6 +37,22 @@ const Hero = () => {
       })
       .to(".right-leaf", { y: 200 }, 0)
       .to(".left-leaf", { y: -200 }, 0);
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    const endValue = isMobile ? "120% top" : "bottom top";
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "video",
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      },
+    });
+    videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      });
+    };
   }, []);
   return (
     <>
@@ -76,7 +94,13 @@ const Hero = () => {
       </section>
 
       <div className="video absolute inset-0">
-        <video muted playsInline preload="auto" src="/videos/output.mp4" />
+        <video
+          ref={videoRef}
+          muted
+          playsInline
+          preload="auto"
+          src="/public/videos/output.mp4"
+        />
       </div>
     </>
   );
